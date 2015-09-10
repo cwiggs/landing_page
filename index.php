10b9a6559fe365cb2a8ca6833ac69656574a9cb1
cwiggs <chrisonline1991@gmail.com>
2015-09-11 02:48:10 +0400

    fix punctuation

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
                                 <img src='/images/me_fountain.jpg' align="right">
 				<h1> Hi, I'm Chris</h1>
 	                                <p>and this is where I live on the Internet. I'm an IT guy, novice programmer, and philosophy enthusiast.</p>
-					<p>I work at Hyperion Works and am responsible for Linux servers, OSX workstations and Windows workstations/servers.  Most of my day is spent helping customers with various computer problems, at night is when I work on the servers (mainly Linux) and focus on programming.</p>
+					<p>I work at Hyperion Works and am responsible for Linux servers, OSX workstations and Windows workstations/servers.  Most of my day is spent helping customers with various computer problems. At night is when I work on servers (mainly Linux) and focus on programming.</p>
 
 <!--				<p>Some of my hobbies are technology, bicycling, hiking/backpacking and camping. I'm really passionate about Linux (GNU/Linux, put down the pitchforks) and open-source software.  I love riding my bike through the city or out in the mountains, I feel more free on a bike compared to in a car. Backpacking is something I really enjoy, I've been to Havasu Falls twice and I'd recommend if you haven't see pictures, do yourself a favor and Google it.</p> -->
 
