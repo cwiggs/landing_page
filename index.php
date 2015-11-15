fa04bbbb1bf449b887ea009ea1dd160dac3ccd16
cwiggs <chrisonline1991@gmail.com>
2015-11-15 22:07:00 +0400

    remove gpg key, added fingerprint to index.php instead.

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php include ('/var/www/cwiggs/includes/header.php')?>
+
                         <div id='intro'>
                                 <img src='/images/me_fountain.jpg' align="right">
 				<h1> Hi, I'm Chris</h1>
 	                                <p>and this is where I live on the Internet. I'm an IT guy, novice programmer, and philosophy enthusiast.</p>
 					<p>I work at Hyperion Works and am responsible for Linux servers, OSX workstations and Windows workstations/servers.  Most of my day is spent helping customers with various computer problems. At night is when I work on servers (mainly Linux) and focus on programming.</p>
 
-<!--				<p>Some of my hobbies are technology, bicycling, hiking/backpacking and camping. I'm really passionate about Linux (GNU/Linux, put down the pitchforks) and open-source software.  I love riding my bike through the city or out in the mountains, I feel more free on a bike compared to in a car. Backpacking is something I really enjoy, I've been to Havasu Falls twice and I'd recommend if you haven't see pictures, do yourself a favor and Google it.</p> -->
 
 				<h2> Hobbies </h2>
 					<ul>
@@ -26,10 +26,12 @@
 			<!-- end 'intro' div-->
 
 			<div id='contact'>
-				<h2> Contact <h2>
-					<p> Email: chrisonline1991 at gmail.com </p>
-					<p> <? $gpg_key = file_get_contents('/var/www/cwiggs/includes/gpg-key.txt'); 
-						#echo $gpg_key; ?> </p>
-
+				<h2> Contact </h2>
+					<ul>
+						<li> Email: chrisonline1991 at gmail.com </li>
+						<li> GPG fingerprint: 8777 8154 26B3 3D3E 1739  FFDD 9934 39CD 7F46 F139 </li>
+					</ul>
+			</div>
+			<!-- end 'contact' div-->
 
 <?php include ('/var/www/cwiggs/includes/footer.php')?>
