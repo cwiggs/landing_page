e14fca608124df78a95a054daa8cf562dd4460a9
cwiggs <chrisonline1991@gmail.com>
2016-01-18 23:51:30 -0700

    edit the look of embeded medium.com

--- blog.php
+++ blog.php
@@ -1,7 +1,7 @@
 <?php include ('/home/cwiggs/development/landing_page/includes/config.php')?>
 <?php include ( $root.'/includes/header.php')?>
 
-<script async src="https://static.medium.com/embed.js"></script><a class="m-profile" href="https://medium.com/@cwiggs">Chris Wiggins</a>
+<script async src="https://static.medium.com/embed.js"></script><a class="m-profile" data-width="100%" data-border="false" href="https://medium.com/@cwiggs">Chris Wiggins</a>
 
 <?php include ( $root.'/includes/footer.php')?>
 
