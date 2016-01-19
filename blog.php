ed343acf5fb4e31e5872eefa6af933cbe2ff0130
cwiggs <chrisonline1991@gmail.com>
2016-01-18 22:32:42 -0700

    embed medium profile in blog.php

---
+++ blog.php
@@ -0,0 +1,6 @@
+<?php include ('/var/www/cwiggs/includes/header.php')?>
+
+<script async src="https://static.medium.com/embed.js"></script><a class="m-profile" href="https://medium.com/@cwiggs">Chris Wiggins</a>
+
+<?php include ('/var/www/cwiggs/includes/footer.php')?>
+
