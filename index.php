e739ddb6000f2c3dac23b1d801ab953be5f9d82e
cwiggs <cwiggins@cwiggs.com>
2016-04-21 16:35:46 -0400

    update index.php

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
                         <div id='intro'>
                                 <img src='/images/me_fountain.jpg' align="right">
 				<h1> Hi, I'm Chris</h1>
-	                                <p>and this is where I live on the Internet. I'm an IT guy, novice programmer, and philosophy enthusiast.</p>
-					<p>I work at Hyperion Works and am responsible for Linux servers, OSX workstations and Windows workstations/servers.  Most of my day is spent helping customers with various computer problems. At night is when I work on servers (mainly Linux) and focus on programming.</p>
+	                                <p>and this is where I live on the Internet. I'm a systems administrator, novice programmer, and philosophy enthusiast.</p>
+					<p>
+						I specialize in Linux systems, and that is where my head is.  However I'm also profecient in Windows and OS X.  My day to day is baby sitting servers and making sure your websites and emails are working properly.
+					</p>
 
 
 				<h2> Hobbies </h2>
@@ -32,8 +34,9 @@
 			<div id='contact'>
 				<h2> Contact </h2>
 					<ul>
-						<li> Email: chrisonline1991 at gmail.com </li>
+						<li> Email: cwiggins at cwiggs.com </li>
 						<li> GPG fingerprint: 8777 8154 26B3 3D3E 1739  FFDD 9934 39CD 7F46 F139 </li>
+						<li>Public SSH Key: <a href="id_rsa.pub">id_rsa.pub</a></li>
 					</ul>
 			</div>
 			<!-- end 'contact' div-->
